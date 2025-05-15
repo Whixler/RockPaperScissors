ebd4c701a83085f3aafa5b26896a813290f98932
Lotte <obdeijn.lotte@gmail.com>
2025-05-15 10:20:20 +0200

    added external JS

---
+++ RPS.js
@@ -0,0 +1,37 @@
+let computerChoice;
+        let getHumanChoice;
+        let getComputerChoice;     
+        let humanScore = 0;
+        let computerScore = 0;
+        let rounds = 5;
+        
+        function playRound(humanChoice, computerChoice){
+            getHumanChoice = prompt("Pick your weapon:Rock, Paper, Scissors: ").toLowerCase();
+            getComputerChoice = Math.floor(Math.random()*3) + 1;
+            if(getComputerChoice === 1){
+            computerChoice = "rock";
+        } else if(getComputerChoice === 2){
+            computerChoice = "paper";
+        } else if(getComputerChoice === 3){
+            computerChoice = "scissors";
+        }
+            if(computerChoice === getHumanChoice ){
+                console.log("You picked the same weapon, no winners this round!");
+            }else if((computerChoice === "rock" && getHumanChoice === "scissors") || 
+            (computerChoice === "paper" && getHumanChoice === "rock") ||
+            (computerChoice === "scissors" && humanChoice === "paper")){
+                console.log(computerChoice + " beats " + getHumanChoice +", you lose!")
+                computerScore++;
+            }else if((computerChoice === "rock" && getHumanChoice === "paper") ||
+            (computerChoice === "paper" && humanChoice === "scissors") ||
+            (computerChoice === "scissors" && humanChoice === "rock")){
+                console.log(getHumanChoice + " beats " + computerChoice +", you win!")
+                humanScore++;
+            }
+            console.log("Human Score: " + humanScore + "  || Computer Score: "+ computerScore);
+            console.log("");
+        }     
+               
+        for (let i = 0; i < rounds; i++){
+            playRound(getHumanChoice, computerChoice);
+        }
